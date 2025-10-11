7f4865a7e730f2c8531b76f03afc5c690b6f44cc
jtx007 <jamesjacobthomas7@gmail.com>
2025-10-11 09:47:35 -0400

    upgrade yarn version and fix errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import App from './routes/App.tsx';
-
+import { ThemeProvider } from './components/theme-provider.tsx';
 const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
     const root = ReactDOM.createRoot(rootElement);
@@ -12,9 +12,14 @@ if (!rootElement.innerHTML) {
         <StrictMode>
             <QueryClientProvider client={new QueryClient()}>
                 <BrowserRouter>
-                    <Routes>
-                        <Route path='/' element={<App />} />
-                    </Routes>
+                    <ThemeProvider
+                        defaultTheme='dark'
+                        storageKey='vite-ui-theme'
+                    >
+                        <Routes>
+                            <Route path='/' element={<App />} />
+                        </Routes>
+                    </ThemeProvider>
                 </BrowserRouter>
             </QueryClientProvider>
         </StrictMode>,
