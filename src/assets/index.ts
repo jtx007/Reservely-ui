1b26439e0b01d3ad5d0a6449bfa8012db8eeee51
jtx007 <jamesjacobthomas7@gmail.com>
2025-10-17 22:30:06 -0400

    assets index

---
+++ src/assets/index.ts
@@ -0,0 +1,23 @@
+import bar from './bar.svg';
+import business from './business.svg';
+import counter from './counter.svg';
+import date from './date.svg';
+import family from './family.svg';
+import meal from './meal.svg';
+import picnic from './picnic.svg';
+import queue from './queue.svg';
+import table from './table.svg';
+import waiter from './waiter.svg';
+
+export {
+    bar,
+    business,
+    counter,
+    date,
+    family,
+    meal,
+    picnic,
+    queue,
+    table,
+    waiter,
+};
