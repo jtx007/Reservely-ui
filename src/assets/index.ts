19890c7dc443ab33ff44a2629ebb02cc94dd9a28
jtx007 <jamesjacobthomas7@gmail.com>
2025-10-20 00:30:53 -0400

    changed svgs to pngs

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,23 +1,23 @@
-import bar from './bar.svg';
-import business from './business.svg';
-import counter from './counter.svg';
-import date from './date.svg';
-import family from './family.svg';
-import meal from './meal.svg';
-import picnic from './picnic.svg';
-import queue from './queue.svg';
-import table from './table.svg';
-import waiter from './waiter.svg';
+import bar from './bar.png';
+import business from './business.png';
+import counter from './counter.png';
+import date from './date.png';
+import family from './family.png';
+import meal from './meal.png';
+import picnic from './picnic.png';
+import queue from './queue.png';
+import table from './table.png';
+import waiter from './waiter.png';
 
 export {
-    bar,
-    business,
-    counter,
-    date,
-    family,
-    meal,
-    picnic,
-    queue,
-    table,
-    waiter,
+  bar,
+  business,
+  counter,
+  date,
+  family,
+  meal,
+  picnic,
+  queue,
+  table,
+  waiter,
 };
