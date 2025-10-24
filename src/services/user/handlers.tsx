96291387c32f62c850448fe47d31a285d0842d44
jtx007 <jamesjacobthomas7@gmail.com>
2025-10-23 20:14:37 -0400

    add getCurrentUserquery

--- src/services/user/handlers.tsx
+++ src/services/user/handlers.tsx
@@ -1,22 +1,109 @@
-export interface UserCreate {
-  username: string;
+export interface UserCreate extends UserLogin {
   email: string;
+}
+
+export interface UserLogin {
+  username: string;
   password: string;
 }
 
+export interface UserUpdate {
+  username?: string;
+  email?: string;
+  password?: string;
+}
+
+interface UserLoginResponse {
+  access_token: string;
+  token_type: 'bearer';
+}
+export interface UserResponse {
+  username: string;
+  email: string;
+  id: number;
+}
+
+const API_URL = import.meta.env.VITE_BASE_API_URL;
 export const userService = {
-  createUser: async (user: UserCreate) => {
-    try {
-      const response = await fetch('http://localhost:8000/users', {
-        method: 'POST',
-        body: JSON.stringify(user),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      return response;
-    } catch (error) {
-      return error;
+  getCurrentUser: async (token: string): Promise<UserResponse> => {
+    const response = await fetch(`${API_URL}/users/current`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.detail || 'Failed to fetch current user');
     }
+
+    const data: UserResponse = await response.json();
+    return data;
+  },
+
+  createUser: async (user: UserCreate): Promise<UserResponse> => {
+    const response = await fetch(`${API_URL}/users`, {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.detail || 'Failed to fetch current user');
+    }
+
+    const data: UserResponse = await response.json();
+    return data;
+  },
+  loginUser: async (user: UserLogin): Promise<UserLoginResponse> => {
+    const response = await fetch(`${API_URL}/login`, {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.detail || 'Failed to fetch current user');
+    }
+
+    const data: UserLoginResponse = await response.json();
+    return data;
+  },
+  updateUser: async (id: number, user: UserUpdate): Promise<UserResponse> => {
+    const response = await fetch(`${API_URL}/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.detail || 'Failed to fetch current user');
+    }
+
+    const data: UserResponse = await response.json();
+    return data;
+  },
+  deleteUser: async (id: number) => {
+    const response = await fetch(`${API_URL}/users`, {
+      method: 'DELETE',
+      body: JSON.stringify(id),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.detail || 'Failed to fetch current user');
+    }
+
+    const data: UserResponse = await response.json();
+    return data;
   },
 };
--- src/services/user/queries.tsx
+++ src/services/user/queries.tsx
@@ -1,7 +1,24 @@
 import { userService } from './handlers';
-import type { UserCreate } from './handlers';
+import type { UserCreate, UserLogin, UserUpdate } from './handlers';
 
 export const createUserMutation = {
   mutationKey: ['create-user'],
   mutationFn: (user: UserCreate) => userService.createUser(user),
 };
+
+export const editUserMutation = {
+  mutationKey: ['update-user'],
+  mutationFn: (id: number, user: UserUpdate) =>
+    userService.updateUser(id, user),
+};
+
+export const loginUserMutation = {
+  mutationKey: ['login-user'],
+  mutationFn: (user: UserLogin) => userService.loginUser(user),
+};
+
+export const getCurrentUserQuery = (token: string) => ({
+  queryKey: ['get-current-user', token],
+  queryFn: () => userService.getCurrentUser(token),
+  enabled: !!token,
+});
